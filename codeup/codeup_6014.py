8690d6333d39b880fa7df48901238ac73a904de5
yudonghoon96 <diehdgns030@naver.com>
2022-07-11 17:41:26 +0900

    파이썬 기초 100제 문제풀이

---
+++ codeup/codeup_6014.py
@@ -0,0 +1,9 @@
+#문제
+#실수(real number) 1개를 입력받아 줄을 바꿔 3번 출력해보자.
+
+#풀이
+n = input()
+n = float(n)
+print(n)
+print(n)
+print(n)
---
+++ codeup/codeup_6016.py
@@ -0,0 +1,7 @@
+#문제
+#공백을 두고 문자(character) 2개를 입력받아 순서를 바꿔 출력해보자.
+
+#풀이
+a, b = input().split()
+print(b)
+print(a)
